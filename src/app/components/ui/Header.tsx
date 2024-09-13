849c3a2dd2c9d16bdf1093a6041be340d94f211e
Sebastian <56268694+Hype02@users.noreply.github.com>
2024-09-13 18:16:50 +0200

    Build fix

---
+++ src/app/components/ui/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="w-full border-b-2 border-b-slate-900 p-1 text-sm bg-stone-900 text-slate-200">Header</div>
+  )
+}
