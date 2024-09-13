1667eeeab5cf1639c5315760e0620f29ac570989
Sebastian <56268694+Hype02@users.noreply.github.com>
2024-09-13 18:11:55 +0200

    Header test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "./components/ui/Header";
 
 
 
@@ -5,8 +6,8 @@ export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     test1237567567
+    <div className="flex flex-col font-[family-name:var(--font-geist-sans)]">
+     <Header/>
     </div>
   );
 }
