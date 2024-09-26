27fcd3f302498611bb13d3b802547227545db2a5
Sebastian <56268694+Hype02@users.noreply.github.com>
2024-09-27 00:45:41 +0200

    Service offline in middleware

---
+++ src/app/serviceOffline/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FaRegSadTear } from 'react-icons/fa'
+
+export default function ServiceOffline() {
+  return (
+<div className="w-screen h-svh max-h-svh flex flex-row justify-center items-center gap-2 text-2xl">
+      serwis jest offline <FaRegSadTear />
+    </div>)
+  
+}
