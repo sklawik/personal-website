b8b714332033197994a2aa1e534cdd1efe78ca32
Sebastian <56268694+Hype02@users.noreply.github.com>
2024-11-10 10:08:08 +0100

    dynamic navbar new variations

--- src/app/components/DynamicHeader.tsx
+++ src/app/components/DynamicHeader.tsx
@@ -1,80 +1,103 @@
-"use client"
+"use client";
 
-import { motion, Variant, Variants } from 'framer-motion'
-
-import React, { useState } from 'react'
+import { motion, useViewportScroll, Variants, Variant, easeInOut, easeIn} from "framer-motion";
+import React, { useState } from "react";
 
 export default function DynamicHeader() {
-
-    const floatingObj =  {
-        position: 'fixed',
-        margin: '0.5rem',
-        marginLeft: '45%',
-        marginRight: '45%',
-        alignItems: 'center',
-        justifyContent: 'center',
+    const floatingObj: Variant = {
+        position: "fixed",
+        margin: "0.5rem",
+        marginLeft: "45%",
+        marginRight: "45%",
+        alignItems: "center",
+        justifyContent: "center",
         transition: {
-            duration: 0.5,
-            ease: 'easeInOut'
+            duration: 0.5, 
+            ease: "easeInOut",
         },
-        borderRadius: '1.5rem',
-        width: 'auto',
-        marginTop: '1rem',
-
-
-
-    }
+        width: "10rem",
+        marginTop: "1rem",
+        borderRadius: '1rem'
+    };
 
     const headerVariants: Variants = {
         static: {
             x: 0,
             y: 0,
-            position: 'fixed',
-            width: '100%',
-
+            position: "fixed",
+            width: "100%",
+            backgroundColor: 'black',
+           padding: '0.5rem',
+            color: 'white',
+            textWrap: 'nowrap',
+            transition:{
+                ease: easeIn,
+                duration: 0.25
+            }
+            
+            
         },
         floating: floatingObj,
         floatingExpanded: {
             ...floatingObj,
-            height: '10rem',
-
-
-            transition:{
-                duration: '1s',
-                ease: 'backInOut'
-                
-            }
+            height: "10rem",
+            backgroundColor: 'black',
+            width: "10rem",
+            transition: {
+                duration: 0.25,
+                ease: "easeInOut",
+            },
+           
+        },
+        floatingFullscreen: {
+            width: '100%',
+            height: '100%'
         }
+    };
+
+    const [currentVariant, setCurrentVariant] = useState("static");
+    const hidden = false;
+
+    function handleNavbarClick() {
+        setCurrentVariant((prev) =>
+            prev === "floating" ? "floatingExpanded" : "floating"
+        );
     }
 
-    const [currentVariant, setCurrentVariant] = useState('static')
 
-    const hidden = false
+    let {scrollY} = useViewportScroll()
 
-    function handleNavbarClick(e: any){
-        if(currentVariant == 'floating'){
-            setCurrentVariant('floatingExpanded')
-        }else{
+    scrollY.on('change', (lastValue)=>{
+        if(lastValue >= 10){
             setCurrentVariant('floating')
+        }else{
+            setCurrentVariant('static')
         }
-       
-    }
+    })
 
     return (
-        hidden ||
-        <motion.div
-            variants={headerVariants}
-            initial='static'
-            animate={currentVariant}
-            onHoverEnd={e=>currentVariant=='floatingExpanded' && setCurrentVariant('floating')}
-        >
-            <section className="flex flex-row text-nowrap w-full px-20 h-full bg-black gap-1 p-2 rounded-xl hover:bg-gray-900 duration-500 select-none"
-            onClick={e=>handleNavbarClick(e)}>
-                <div onClick={() => setCurrentVariant('floating')}>animate 1</div>
-                <div onClick={() => setCurrentVariant('static')}>animate 1</div>
-            </section>
-
-        </motion.div>
-
-    )
+        hidden || (
+            <motion.div
+                variants={headerVariants}
+                initial="static"
+                animate={currentVariant}
+                onHoverEnd={() =>
+                    currentVariant === "floatingExpanded" &&
+                    setCurrentVariant("floating")
+                }
+            >
+                <section
+                    className="flex flex-row gap-1 duration-500 select-none"
+                    onClick={handleNavbarClick}
+                >
+                    <div onClick={() => setCurrentVariant("floating")}>
+                        animate 1
+                    </div>
+                    <div onClick={() => setCurrentVariant("static")}>
+                        animate 2
+                    </div>
+                </section>
+            </motion.div>
+        )
+    );
 }
