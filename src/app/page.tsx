2830773fc704b128572106232076f28be8b523f3
Sebastian <56268694+Hype02@users.noreply.github.com>
2024-12-05 11:08:22 +0100

    margin fix for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export const dynamic = 'force-dynamic';
 export default async function Home() {
 
   return (
-    <div className=" flex flex-col font-[family-name:var(--font-geist-sans)] justify-center items-center ml-96 mr-96">
+    <div className=" flex flex-col font-[family-name:var(--font-geist-sans)] justify-center items-center sm:ml-96 sm:mr-96">
 
 
       <div className="flex flex-col flex-grow w-full h-svh ">
-        <section className="flex-grow">
+        <section className="flex-grow min-h-full  bg-green-500">
           123
         </section>
-        <section className="flex-grow">
+        <section className="flex-grow min-h-full  bg-red-500">
           123
         </section>
       </div>
