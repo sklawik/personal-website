c0c97ab4b36836df799067c0c5a2b70a099d4fb1
root <root@homepc.>
2025-01-05 23:22:19 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default async function Home() {
 
   return (
     <div className=" flex flex-col font-[family-name:var(--font-geist-sans)] justify-center items-center sm:ml-96 sm:mr-96 snap ">
-
+      test
 
       <div className="flex flex-col flex-grow w-full min-h-screen  snap-start">
 
