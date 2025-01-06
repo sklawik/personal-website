4135156b11db87e1217ab18b146466271f6119fb
root <root@homepc.>
2025-01-06 19:08:49 +0100

    info box on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ export default async function Home() {
 
   return (
     <div className=" flex flex-col font-[family-name:var(--font-geist-sans)] justify-center items-center sm:ml-96 sm:mr-96 snap ">
-      test
+     
 
-      <div className="flex flex-col flex-grow w-full min-h-screen  snap-start">
-
+      <div className="flex flex-col flex-grow w-full min-h-screen  snap-start justify-center items-center dark:text-white">
+      <h2>Strona internetowa jest w ciągłej budowie..</h2>
+    <h4>Stworzona na bazie NextJS15, zawierać będzie dynamiczny, interaktywny nagłówek oddziałowujący na elementy strony www.</h4>  
+      <br/>
+     <a href="https://github.com/sklawik/personal-website" className="underline text-black dark:text-white">kod źródłowy</a> 
       </div>
+     
       <div className="flex flex-col flex-grow w-full min-h-screen snap-end ">
-
+  
       </div>
       <Footer/>
     </div>
