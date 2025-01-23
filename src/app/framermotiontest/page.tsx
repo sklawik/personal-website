24af5da47ce0c55ecc4c2f156d64a780850b9f46
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-01-23 21:50:59 +0100

    animation tests with useState callback value

--- src/app/framermotiontest/page.tsx
+++ src/app/framermotiontest/page.tsx
@@ -1,29 +1,67 @@
 "use client"
 
-import React from 'react'
+import { AnimationType, motion, Variant } from 'framer-motion'
+import React, { ReactElement, useRef, useState } from 'react'
 
 
 
-const Cursor = () =>{
-  return <section className="absolute z-0 h-7 rounded-full bg-black md:h-12  mt-1 ml-1 top-0 left-0 ">
-    </section>
+const Cursor = (props: any ) => {
+  return <motion.section
+    animate={props.position}
+
+    className="absolute z-0 h-7 rounded-full bg-black md:h-12  top-0 left-0 ">
+
+  </motion.section>
+}
+
+const ListItem = (props: any) => {
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  return <div className="mix-blend-difference  z-10 cursor-pointer px-2 py-1"
+    onMouseLeave={() => {
+      props.setPosition((pv: any)=>({
+        ...pv,
+        opacity: 0,
+        scale: 0,
+        
+      }))
+    }}
+    onMouseEnter={(e) => {
+      if (!ref.current) return
+      const data = ref.current.getBoundingClientRect();
+      props.setPosition({
+        opacity: 1,
+        width: data.width,
+        left: ref.current.offsetLeft
+      })
+    }}
+    ref={ref}>
+    {props.children}
+  </div>
 }
 
 export default function page() {
 
-
+  const [position, setPosition] = useState<Variant>(
+    {
+      left: 0,
+      width: 0,
+      opacity: 0
+    }
+  )
 
   return (
     <div className="flex-grow  w-screen h-screen flex justify-center items-center ">
-        <nav className="flex flex-row gap-2 w-auto rounded-full border-gray-800 uppercase border-2 text-white  p-2 [&_div]:px-2 [&_div]:py-1
-        relative div:bg-red-500 [&_div]:z-10 [&_div]:m-1 [&_div]:mix-blend-difference">
-          <div className="">home</div>
-          <div>pricing</div>
-          <div>features</div>
-          <div>docs</div>
-          <div>blog</div>
-          <Cursor/>
-        </nav>
-    </div>  
+      <nav className="flex flex-row gap-2 w-auto rounded-full border-gray-800 uppercase border-2 text-white 
+        relative p-2 ">
+        <ListItem setPosition={setPosition}>Home</ListItem>
+        <ListItem setPosition={setPosition}>Pricing</ListItem>
+        <ListItem setPosition={setPosition}>Features</ListItem>
+        <ListItem setPosition={setPosition}>Docs</ListItem>
+        <ListItem setPosition={setPosition}>Blog</ListItem>
+        <Cursor position={position} />
+      </nav>
+    </div>
   )
 }
