ce5b7bab0f9f0a76d566aa64e20b432cf1f75d9b
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-06-25 00:27:14 +0200

    added folders

---
+++ src/app/spedktorbday/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className="w-svw h-svh bg-black flex flex-row justify-center items-center">
+            <div>
+                <video src="/1.mp4" loop={true} autoPlay={true} className="w-full h-full"></video>
+            </div>
+        </div>
+    )
+}
