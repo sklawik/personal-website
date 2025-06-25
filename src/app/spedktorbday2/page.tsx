4b235f106636702a9e6087bf69b367339536f384
sklawik <sklawik@MacBook-Air-Sebastian.local>
2025-06-25 07:56:30 +0200

    vid change:

--- src/app/spedktorbday2/page.tsx
+++ src/app/spedktorbday2/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
     return (
         <div className="w-svw h-svh bg-black flex flex-row justify-center items-center">
             <div>
-                <video src="/2.mp4" controls loop={true} autoPlay={true} className="w-full h-full"></video>
+                <video src="/2.mp4" controls  muted={true} loop={true} autoPlay={true} className="w-full h-full"></video>
             </div>
         </div>
     )
